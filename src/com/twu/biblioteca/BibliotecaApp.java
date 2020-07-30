5d5cb972a3f759cf1bd8b88225ccd5adcf1fa625
Blank <zilisheng1996@gmail.com>
2020-07-30 12:08:14 +0800

    welcome message ok

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,10 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    private static final String WELCOME = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(WELCOME);
     }
+
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author: Blank
+ * @description: com.twu.biblioteca
+ * @date: 7/30/20
+ * @version: 1.0
+ */
+public class BibliotecaAppTest {
+    private static final String WELCOME = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n";
+
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+        System.setErr(System.err);
+    }
+    @Test
+    public void wellcomeTest() {
+        BibliotecaApp.main(null);
+        assertEquals(WELCOME, outContent.toString());
+    }
+}
